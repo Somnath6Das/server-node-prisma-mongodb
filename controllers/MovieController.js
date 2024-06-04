9ec96a6879b63dfe066bb2e3b56ef1f940e8cd56
Somnath6das <somnath8das@gmail.com>
2024-06-04 20:39:17 +0530

    update

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,20 +1,68 @@
 import prisma from "../config/db.config.js";
 
-const store = async (req, res) => {
-  const { name } = req.body;
-  const findMovie = await prisma.movie.findUnique({
-    where: {
-      name,
-    },
-  });
-  if (findMovie) {
-    return res.status(401).json({ message: "Movie Already exists"});
-  }
-  if (!findMovie) {
-    const movie = await prisma.movie.create({
-      data: { name },
+export const index = async (req, res) => {
+  try {
+    const movies = await prisma.movie.findMany({
+      select: {
+        name: true,
+      },
     });
-    return res.status(201).json({ message: "Movie added successfully", movie });
-  }
+    return res.status(200).json({ movies });
+  } catch (err) {}
 };
-export default store;
+
+export const create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).json({ message: "Name is empty" });
+    }
+    const findMovie = await prisma.movie.findUnique({
+      where: {
+        name,
+      },
+    });
+    if (findMovie) {
+      return res.status(401).json({ message: "Movie Already exists" });
+    }
+    if (!findMovie) {
+      const movie = await prisma.movie.create({
+        data: { name },
+      });
+      return res
+        .status(201)
+        .json({ message: "Movie added successfully", movie });
+    }
+  } catch (err) {}
+};
+
+export const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).json({ message: "Name is empty" });
+    }
+    await prisma.movie.update({
+      data: {
+        name,
+      },
+      where: {
+        id,
+      },
+    });
+    return res.status(200).json({ message: "Movie updated successfully" });
+  } catch (err) {}
+};
+
+export const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await prisma.movie.delete({
+      where: {
+        id,
+      },
+    });
+    return res.status(200).json({ message: "Movie deleted" });
+  } catch (err) {}
+};
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,9 +1,11 @@
-import { Router } from "express";  
-import store from "../controllers/MovieController.js";
+import { Router } from "express";
+import { create, index, remove, update } from "../controllers/MovieController.js";
 import authMiddleware from "../middleware/AuthMiddleware.js";
 const router = Router();
 
-router.post("/movie", authMiddleware, store);
-
+router.post("/movie", authMiddleware, create);
+router.get("/movie", authMiddleware, index);
+router.put("/movie/:id", authMiddleware, update);
+router.delete("/movie/:id", authMiddleware, remove);
 
 export default router;
